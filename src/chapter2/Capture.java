b4fcc9b82abfd132d7ebbcdb09287f9a30ca5534
pigeon2gugu <khn1135@gmail.com>
2024-05-08 21:37:50 +0900

    feat : 함수형 자바 2장 구현

---
+++ src/chapter2/Capture.java
@@ -0,0 +1,50 @@
+package chapter2;
+
+import java.time.Period;
+import java.util.function.Supplier;
+
+//파라미터가 아닌 람다 외부에서 정의된 변수를 람다식 내부에 저장하고 사용
+public class Capture {
+
+    static int staticAnswer = 42;
+    private int instanceAnswer = 42;
+    static void capture() {
+        int theAnswer = 42;
+        Runnable printAnswer = () -> System.out.println("the answer is " + theAnswer);
+        printAnswer.run();
+    }
+
+    //캡처되는 지역 변수 (스태틱 영역)는 final이거나 effective final이어야 한다. (compile error)
+    //지역 변수가 할당 해제 된 후에도 람다에서 사용 가능
+//    public static void capture2() {
+//        int theAnswer = 42;
+//        Runnable changedAnswer = () -> {
+//            theAnswer = 43;
+//            System.out.println(theAnswer);
+//        };
+//        changeAnswer.run();
+//    }
+
+    //static 필드나 인스턴스 필드는 final 제약 x (힙 혹은 메서드 영역)
+    static void capture3() {
+        Runnable changedAnswer = () -> {
+            staticAnswer = 43;
+            System.out.println("the changed answer is " + staticAnswer);
+        };
+        changedAnswer.run();
+    }
+
+    void capture4() {
+        Runnable changedAnswer = () -> {
+            this.instanceAnswer = 43;
+            System.out.println("the changed answer is " + this.instanceAnswer);
+        };
+        changedAnswer.run();
+    }
+
+    public static void main(String[] args) {
+        capture();
+        capture3();
+    }
+}
+
---
+++ src/chapter2/LambdaPractice.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+import java.util.function.Predicate;
+
+public class LambdaPractice {
+    interface LikePredicate<T> {
+        boolean test(T value);
+    }
+
+    static LikePredicate<String> isNull = value -> value == null;
+    static Predicate<String> isNull2 = value -> {
+            return value == null;
+    };
+    //compile error
+    //Predicate<String> wontCompile = isNull;
+
+    public static void main(String[] args) {
+        System.out.println(isNull.test("isNull"));
+        System.out.println(isNull2.test("isNull"));
+    }
+}
